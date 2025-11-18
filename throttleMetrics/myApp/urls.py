fdfce0b0e61778964a7c5f13edc929f71b12ad65
unknown <yt65@rice.edu>
2025-11-18 12:50:32 -0600

    Continued

--- throttleMetrics/myApp/urls.py
+++ throttleMetrics/myApp/urls.py
@@ -22,4 +22,5 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", views.index, name="index"),
+    path("down-but-not-out/", views.post, name="post"),
 ]
--- throttleMetrics/myApp/views.py
+++ throttleMetrics/myApp/views.py
@@ -4,3 +4,6 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, "myApp/index.html", {})
+
+def post(request):
+    return render(request, "myApp/post.html", {})
