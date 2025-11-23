d6a01d538e84fb64cc9ba359196143c5c5129f3f
unknown <yt65@rice.edu>
2025-11-22 21:56:31 -0600

    Troubleshooting deployment

--- throttleMetrics/throttleMetrics/settings.py
+++ throttleMetrics/throttleMetrics/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ow7(3kp1wci=%202k4tbw!o6llcg0&*ikxrrf9t+o^$lo$3hq!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [".vercel.app"]
 
