693d85130ad4009e104c959861485079b05131ea
unknown <yt65@rice.edu>
2025-11-22 20:44:41 -0600

    Finished basic setup and first post. Ready for deployment.

--- throttleMetrics/myApp/urls.py
+++ throttleMetrics/myApp/urls.py
@@ -22,7 +22,7 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", views.index, name="index"),
-    path("down-but-not-out/", views.post1, name="post1"),
+    path("Power-Unit-Pick-Me-Up/", views.post1, name="post1"),
     path("about/", views.about, name="about"),
     path("contact/", views.contact, name="contact")
 ]
